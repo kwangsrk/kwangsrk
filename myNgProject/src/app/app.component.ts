780278e133b90e97fc451af61603dab3a60192e7
Kwangsrk <gvangnos@gmail.com>
2019-03-30 14:09:33 +0700

    work2na

--- myNgProject/src/app/app.component.ts
+++ myNgProject/src/app/app.component.ts
@@ -6,49 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myNgProject';
-
-  name: string = "Miss u na";
-  score: number = 99;
-
-  student = {
-    name: "Kwang",
-    studentID: "5921602795",
-    weight: 51,
-    height: 171
-  }
-  studentlist = [{
-    name: "Kwang",
-    studentID: "5921602795",
-    weight: 51,
-    height: 171
-  },
-  {
-    name: "Kwang",
-    studentID: "5921602795",
-    weight: 47,
-    height: 171
-  },
-  {
-    name: "Kwang",
-    studentID: "5921602795",
-    weight: 80,
-    height: 171
-  }
-
-
-
-
-
-  ]
+ productlist=[{
+   pID : "0001",
+   pName : "แปรงสีฟัน",
+   pCost : 200
+ },{
+   pID: "0002",
+   pName :"แก้วน้ำ",
+   pCost : 10
+ },{
+   pID : "0003",
+   pName : "ยาสีฟัน",
+   pCost : 20 
+ }];
+  
+  
   constructor() {
-    this.studentlist.map((object, index) => {
-       let obj:any =object;
-         obj.bmi = object.weight/((object.height/100)*(object.height/100))
-         return obj;
-      let student = "student in function";
-      console.log(this.studentlist);
-      console.log(student);
-    })
+    
+    }
   }
-}
